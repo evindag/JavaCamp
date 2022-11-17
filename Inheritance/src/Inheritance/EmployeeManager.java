449b4e5b3309cc386bef465cd7c39316b366e93f
evindag <80323753+evindag@users.noreply.github.com>
2022-11-17 20:48:52 +0300

    Create EmployeeManager.java

---
+++ Inheritance/src/Inheritance/EmployeeManager.java
@@ -0,0 +1,13 @@
+package Inheritance;
+
+public class EmployeeManager extends PersonManager{
+	public void List(){
+		System.out.println("Listed");
+	}
+	public void Add(){
+		System.out.println("Added");
+	}
+	public void BestEmployee() {
+		System.out.println("The Best Employee!");
+	}
+}
