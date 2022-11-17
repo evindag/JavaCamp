2b52766165e59e8d82f000b41c2fdaee37b6469e
evindag <80323753+evindag@users.noreply.github.com>
2022-11-17 22:43:46 +0300

    Create Main.java

---
+++ inheritanceProject/src/inheritanceProject/Main.java
@@ -0,0 +1,10 @@
+package inheritanceProject;
+
+public class Main {
+
+	public static void main(String[] args) {
+		CreditUI creditUI = new CreditUI();
+		creditUI.CreditCalculate(new TeacherCreditManager());
+	}
+
+}
