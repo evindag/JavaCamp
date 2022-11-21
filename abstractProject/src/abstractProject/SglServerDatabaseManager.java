368d095141cccbfe605fa6773ee79aaff88f81f7
evindag <80323753+evindag@users.noreply.github.com>
2022-11-21 10:14:06 +0300

    for SqlServer

---
+++ abstractProject/src/abstractProject/SglServerDatabaseManager.java
@@ -0,0 +1,10 @@
+package abstractProject;
+
+public class SglServerDatabaseManager extends BaseDatabaseManager{
+
+	@Override
+	public void getData() {
+		System.out.println("Data Brought : SqlServer");
+	}
+
+}
