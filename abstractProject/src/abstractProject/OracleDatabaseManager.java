5884521946eb36abcd4e973f8bd276a2ca422ff5
evindag <80323753+evindag@users.noreply.github.com>
2022-11-21 10:13:57 +0300

    for Oracle

---
+++ abstractProject/src/abstractProject/OracleDatabaseManager.java
@@ -0,0 +1,10 @@
+package abstractProject;
+
+public class OracleDatabaseManager extends BaseDatabaseManager{
+
+	@Override
+	public void getData() {
+		System.out.println("Data Brought : Oracle");
+	}
+
+}
