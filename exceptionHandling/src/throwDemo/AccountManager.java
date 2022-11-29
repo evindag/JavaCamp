7769f7e63b7f7031b6d72ec5918ddcf679835624
evindag <80323753+evindag@users.noreply.github.com>
2022-11-30 00:33:48 +0300

    Create AccountManager.java

---
+++ exceptionHandling/src/throwDemo/AccountManager.java
@@ -0,0 +1,19 @@
+package throwDemo;
+
+public class AccountManager {
+	private double balance;
+	public void deposit(double amount) {
+		balance = getBalance() + amount;
+	}
+	public void withdraw(double amount) throws BalanceInsufficentException {
+		if(balance>=amount) {
+			balance = getBalance() - amount;
+		}else {
+			throw new BalanceInsufficentException("Insufficient Balance :(");
+		}
+		
+	}
+	public double getBalance() {
+		return balance;
+	}
+}
